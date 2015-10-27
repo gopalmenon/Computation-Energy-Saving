b39d373367072a39934fe3f51f9e6703afed5b1d
gopalmenon <gopal.mnn@gmail.com>
2015-10-26 22:16:34 -0600

    Separated out the matrix creation into functions so that storage is released when function ends. This way, storage used by one matrix will not slow down the creation of the other one.

--- Computation Enery Saving/Computation Enery Saving/MatrixClient.cpp
+++ Computation Enery Saving/Computation Enery Saving/MatrixClient.cpp
@@ -4,22 +4,35 @@
 #include <chrono>
 #include <iostream>
 
-int main() {
+const int NUMBER_OF_ROWS = 10000, NUMBER_OF_COLUMNS = 11000;
+std::chrono::milliseconds runTime1InMilliseconds, runTime2InMilliseconds;
 
-	const int NUMBER_OF_ROWS = 10000, NUMBER_OF_COLUMNS = 11000;
+void createSerialMatrix() {
 
 	std::cout << "Serial Matrix" << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
 	SerialMatrix serialMatrix(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
-	std::chrono::milliseconds runTime1InMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
-	std::cout << "Serial Matrix initialization took " << runTime1InMilliseconds.count() << " ms."<< std::endl;
+	runTime1InMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
+	std::cout << "Serial Matrix initialization took " << runTime1InMilliseconds.count() << " ms." << std::endl;
 	//serialMatrix.showMatrix();
 
+}
+
+void createParallelMatrix() {
+
 	std::cout << "Parallel Matrix" << std::endl;
-	start = std::chrono::high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
 	ParallelMatrix parallelMatrix(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
-	std::chrono::milliseconds runTime2InMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
+	runTime2InMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
 	std::cout << "Parallel Matrix initialization took " << runTime2InMilliseconds.count() << " ms." << std::endl;
 	std::cout << "Speedup was " << runTime1InMilliseconds.count() * 1.0 / runTime2InMilliseconds.count() << std::endl;
 	//parallelMatrix.showMatrix();
+
+}
+
+int main() {
+
+	createSerialMatrix();
+	createParallelMatrix();
+
 }
